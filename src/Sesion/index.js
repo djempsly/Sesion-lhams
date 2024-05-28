58da0c3ea45ef7fbdd2b07b38199d197ae9c53ef
Jeffry Almonte Dely <djempsly120@gmail.com>
2024-05-28 08:17:08 -0400

    Ultima actualizacion

--- src/Sesion/index.js
+++ src/Sesion/index.js
@@ -1,66 +1,18 @@
 import React from "react"
-import { Field, Form, Formik } from "formik"
-
-const Sesion = () =>{
-
-    return(
-        <>
-
-<Formik
-      initialValues={{ email: '', password: '' }}
-      validate={values => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <Field type="email" name="email" />
-          {/* <ErrorMessage name="email" component="div" /> */}
-          <Field type="password" name="password" />
-          {/* <ErrorMessage name="password" component="div" /> */}
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
-      )}
-    </Formik>
+import './sesion.css'
 
 
+const Sesion= ()=>{
+  return(
+    <>
+    <div className="container-sesion">
+    <p className="letras"> Disculpe lo inconveniente,  <br/> esta parte está en construcción</p>
+  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        </>
-    )
+      {/* <img className="img-construccion" src={construccion} alt="construccion" /> */}
+    </div>
+    </>
+  )
 }
 
-
-export {Sesion}
+export { Sesion}
